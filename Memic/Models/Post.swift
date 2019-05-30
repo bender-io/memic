b3518316ade47018ab3721c3690a6df1bd17d47c
Brian Hersh <bhersh@amp-bhersh.local>
2019-05-30 14:51:01 -0600

    added Post model

---
+++ Memic/Models/Post.swift
@@ -0,0 +1,37 @@
+//
+//  Question.swift
+//  Memic
+//
+//  Created by Brian Hersh on 5/30/19.
+//  Copyright © 2019 Brian Daniel. All rights reserved.
+//
+
+import UIKit
+
+class Post {
+    
+    let profileImage : UIImage?
+    let username : String
+    let timestamp : Date
+    let memes : [Meme?]
+    
+    init(profileImage : UIImage = #imageLiteral(resourceName: "profile_pic"), username: String, timestamp: Date = Date(), memes: [Meme?]) {
+        self.profileImage = profileImage
+        self.username = username
+        self.timestamp = timestamp
+        self.memes = memes
+    }
+}
+
+struct Meme {
+    let image : UIImage?
+    let like : Int
+    let rank : Rank
+    
+    enum Rank : String {
+        case gold
+        case silver
+        case bronze
+        case none
+    }
+}
